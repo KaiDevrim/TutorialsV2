c7fdc32f4e796a94a09d3898e99c3aa126c7e6dc
Kai Devrim <kkgit@devrim.tech>
2021-03-06 17:27:20 -0800

    Start work on the Express part. 42 minutes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post"
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
+
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send("Hello World");
+  })
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  })
 };
 
 main().catch((err) => {
